99e3256948077b9cdd9d57493ae58a8ba281efff
Mathyxd <mathias3730@gmail.com>
2026-04-22 09:47:17 +0200

    fish og aquarium

--- src/main/java/AquariumSystem/interfaces/Fish.java
+++ src/main/java/AquariumSystem/interfaces/Fish.java
@@ -1,4 +1,6 @@
 package AquariumSystem.interfaces;
 
 public interface Fish {
+    String getName();
+    void registerHealthCheck(String note);
 }
--- src/main/java/AquariumSystem/simpleaquarium/SimpleFish.java
+++ src/main/java/AquariumSystem/simpleaquarium/SimpleFish.java
@@ -1,4 +1,22 @@
 package AquariumSystem.simpleaquarium;
 
+import AquariumSystem.interfaces.Fish;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleFish {
+    private String name;
+    private List<String> HealthNotes = new ArrayList<>();
+
+    public SimpleFish(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void registerHealthCheck(String note) {
+        healthNotes.add(note);
+    }
 }
